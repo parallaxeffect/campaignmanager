23cbdaa2625fb726f91dcae40b0a235cb9ba3050
t <y@y.com>
2017-05-09 11:36:42 -0400

    Set up node_modules sym-link

--- provision.sh
+++ provision.sh
@@ -2,3 +2,8 @@ apt-get update
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_6.x | sh
 apt-get install -y nodejs
+
+# Create sym-link so node_modules aren't shared to host
+mkdir node_modules
+cd /vagrant
+ln -s /home/vagrant/node_modules node_modules
