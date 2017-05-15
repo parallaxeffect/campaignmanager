487eaf7dc7288e7d45fca94ae7b8aef54bd81691
t <y@y.com>
2017-05-15 02:56:01 -0400

    Add local persist

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 import {Provider} from 'react-redux';
+import {persistStore, autoRehydrate} from 'redux-persist';
 
 import {reducer} from './reducer.js'
 import {TopicList} from './TopicList.jsx'
 import {AddTopic} from './AddTopic.jsx'
 import {Views} from './Views.jsx'
 
-const store = createStore(reducer);
+const store = (autoRehydrate())(createStore)(reducer);
+persistStore(store);
 
 class App extends React.Component {
 	render () {
